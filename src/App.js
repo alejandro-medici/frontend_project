d4ca8179aa07c4ab3ba7b80b9d4ccdbbcf4bcc8e
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2022-11-10 20:14:35 -0300

    warning removed

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import Router from './Routes/Router';
 import NavBar from './Components/Navbar/Navbar';
 import Footer from './Components/footer-components/Footer';
 import CTAbanner from './Components/footer-components/CTAbanner';
-import Headerbar from './Components/featured-components/Headerbar';
-import UserInfo from './Components/featured-components/UserInfo';
-import UserPrompt from './Components/featured-components/UserPrompt';
+//import Headerbar from './Components/featured-components/Headerbar';
+//import UserInfo from './Components/featured-components/UserInfo';
+//import UserPrompt from './Components/featured-components/UserPrompt';
 import Toggle from './Components/Interruptor';
 //<a href='/Home' target="_blank">Voy al Home</a>
 
 function App(props) {
-  const loggedIn = props.loggedIn
+  //const loggedIn = props.loggedIn
 
   function myAlert() {
     alert("Se apreoto Boton!")
