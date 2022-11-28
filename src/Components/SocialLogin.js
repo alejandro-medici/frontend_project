f8fd786fc6f8ea9d0e94e37ee5e13a2b3004f52c
Alejandro Medici <alejandro.medici@nextiteration.com.ar>
2022-11-28 20:17:57 -0300

    Firestore added

--- src/Components/SocialLogin.js
+++ src/Components/SocialLogin.js
@@ -1,4 +1,5 @@
-import { app } from "../Firebase/FirebaseApp";
+import { app, db } from "../Firebase/FirebaseApp";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { getAuth, signInWithPopup, linkWithPopup } from "firebase/auth";
 import { GoogleAuthProvider, TwitterAuthProvider } from "firebase/auth";
 
@@ -13,6 +14,25 @@ export default function SocialLogin (props) {
         .then( (credentials) => {
             const firebase_user = credentials.user;
             console.log(firebase_user);
+            // Aca hacemos una query de la collection Pokemons
+            const pokemonsRef = collection(db, "Pokemons");
+            const q = query(pokemonsRef, where("name", "==", "pikachu"));
+
+            // Obtenemos los datos de la collecion entera o de lo filtrado por la query
+            const docs = getDocs(pokemonsRef)
+            .then( (result ) => {
+                  console.log(result);
+                  result.docs.forEach((doc) => {
+                    console.log(`${doc.id} => ${doc.data()}`);
+                  });
+    
+            }).catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode);
+                console.log(errorMessage);
+            } );
+           
         })
         .catch( (error) => {
             const errorCode = error.code;
--- src/Firebase/FirebaseApp.js
+++ src/Firebase/FirebaseApp.js
@@ -1,17 +1,30 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+/*const firebaseConfig = {
   apiKey: "AIzaSyD_qHBDVsGIu79ryuekEUB9q31s_XJ-9Wg",
   authDomain: "front-end-22801.firebaseapp.com",
   projectId: "front-end-22801",
   storageBucket: "front-end-22801.appspot.com",
   messagingSenderId: "884002254420",
   appId: "1:884002254420:web:ad9c8ce642eb33ed1647c1"
+};*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBbH3xixzya2HF7QcxzSuf3g4-EBE5mAR4",
+  authDomain: "front-end-82c91.firebaseapp.com",
+  projectId: "front-end-82c91",
+  storageBucket: "front-end-82c91.appspot.com",
+  messagingSenderId: "895633365015",
+  appId: "1:895633365015:web:2286c154584e3934266452",
+  measurementId: "G-XZVM8LEY65"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app)
